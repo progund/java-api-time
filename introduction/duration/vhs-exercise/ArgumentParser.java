b7b9e834fede0da4bbc0cd26799483bf40806fa0
Rikard Fröberg <rikard@morus.se>
2018-10-09 12:54:07 +0200

    Added some javadoc

--- introduction/duration/vhs-exercise/ArgumentParser.java
+++ introduction/duration/vhs-exercise/ArgumentParser.java
@@ -1,6 +1,18 @@
+/*
+ * Copyright 2018 Rikard Fröberg and Henrik Sandklef, Juneday education
+ * a part of Morus konsult AB.
+ */
+
 import java.time.LocalTime;
 import java.time.format.DateTimeParseException;
 
+/**
+ * Parses the arguments for the VHSOO application and provides
+ * error message and exit code for any parse error (including
+ * wrong number of arguments), and provides the arguments as the
+ * correct parsed type.
+ * @author Rikard Fröberg
+ */
 public class ArgumentParser {
 
   private StringBuilder errorMessage;
@@ -9,7 +21,14 @@ public class ArgumentParser {
   private LocalTime start;
   private LocalTime end;
   private int exitCode;
-  
+
+  /**
+   * Constructs a new ArgumentParser and parses the arguments.
+   *
+   * Parses the arguments if the length of the argument array is
+   * correct.
+   * @param args The arguments as an array of String
+   */
   public ArgumentParser(String[] args) {    
     errorMessage = new StringBuilder();
     checkArgsLength(args.length);
@@ -21,6 +40,11 @@ public class ArgumentParser {
     }
   }
 
+  /**
+   * Checks that the length of the argument is 4, creates a USAGE string
+   * as the errorMessage and sets the exit code to 1 otherwise.
+   * @param length The length of the arguments as an int
+   */
   private void checkArgsLength(int length) {
     if (length != 4) {
       errorMessage.append("Usage: java VHS <length> <used> <start-time> <end-time>\n");
@@ -35,12 +59,22 @@ public class ArgumentParser {
       exitCode = 1;
     }
   }
-  
+
+  /**
+   * Checks whether the parsing ran into any parse errors.
+   * @return true if any errors were encountered, false otherwise
+   */
   public boolean hasParseErrors() {
     return errorMessage.length() != 0;
   }
 
 
+  /**
+   * Returns the exit code based on how successful the parsing was.
+   * @return The exit code as an int. 0 if the parsing was successful,
+   *         1 if there was wrong number of arguments, 2 if any argument
+   *         wouldn't parse.
+   */
   public int exitCode() {
     return exitCode;
   }
